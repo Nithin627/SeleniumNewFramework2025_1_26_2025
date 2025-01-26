bdf008812a3b36fa62bee9be39d4c1df93cf9e26
Nithin <nithinpaddyur@gmail.com>
2025-01-26 17:06:13 +0530

    second commit 1/26/2025

--- src/test/java/com/nithin/tests/LoginPageTest.java
+++ src/test/java/com/nithin/tests/LoginPageTest.java
@@ -1,9 +1,12 @@
 package com.nithin.tests;
 
 import com.nithin.driver.DriverManager;
+import com.nithin.utils.ReadPropertyFile;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public final class LoginPageTest extends BaseTest{
 
@@ -12,8 +15,9 @@ public final class LoginPageTest extends BaseTest{
     }
 
     @Test
-    public void test1(){
-        DriverManager.getDriver().findElement(By.name("q")).sendKeys("foot Ball", Keys.ENTER);
+    public void test1() throws IOException {
+//        DriverManager.getDriver().findElement(By.name("q")).sendKeys("foot Ball", Keys.ENTER);
+        DriverManager.getDriver().findElement(By.name(ReadPropertyFile.getValue("SEARCH_BAR"))).sendKeys("foot Ball", Keys.ENTER);
         System.out.println("Login Page Test 1");
     }
 
